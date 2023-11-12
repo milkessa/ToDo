64b7a6b1b6cdeef2a456fa6ed6e8a755e87ebd47
milkessa <38739690+milkessa@users.noreply.github.com>
2023-11-12 14:50:30 -0500

    mod

---
+++ src/components/Tasks/index.tsx
@@ -0,0 +1,9 @@
+import styles from './tasks.module.css';
+
+export function Task(){
+    return(
+        <section className={styles.tasks}>
+
+        </section>
+    )
+}
--- src/components/headers/index.tsx
+++ src/components/headers/index.tsx
@@ -1,8 +1,16 @@
 import styles from './header.module.css'
+import { AiOutlinePlusCircle } from 'react-icons/ai';
 export function Header(){
     return(
         <header className={styles.header}>
             <p>header</p>
+            <form action="" className={styles.newTaskForm}>
+                <input placeholder='add a new task' type='text' />
+                <button>
+                    Create
+                    <AiOutlinePlusCircle size={20}/>
+                </button>
+            </form>
         </header>
     )
 }
